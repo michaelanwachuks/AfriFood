4547cd5cddc225f374c8e5c98036d4747897602d
Michael <michaelanwachuks@gmail.com>
2026-03-19 22:30:06 +0100

    update user entity

--- backend/src/main/java/com/afrifoodApp/model/UserEntity.java
+++ backend/src/main/java/com/afrifoodApp/model/UserEntity.java
@@ -1,7 +1,19 @@
-import java.util.Date;
+package com.afrifoodApp.model;
 
+import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String username;
     private String email;
     private String phone;
---
+++ backend/src/main/java/com/afrifoodApp/model/UserRepository.java
@@ -0,0 +1,9 @@
+package com.afrifoodApp.model;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+ 
+
+public interface UserRepository  extends JpaRepository<UserEntity, Long>    {
+
+    
+}
