1ba493e0a89142923326cadbce9b44ff2cab8084
Michael <michaelanwachuks@gmail.com>
2026-03-19 21:35:24 +0100

    Add user entity class

---
+++ backend/src/main/java/UserEntity.java
@@ -0,0 +1,87 @@
+import java.util.Date;
+
+public class UserEntity {
+    private Long id;
+    private String username;
+    private String email;
+    private String phone;
+    private String password;
+    private Date createdAt;
+    private Date updatedAt;
+
+    //create emply constructor
+    public UserEntity() {
+    }
+
+    //create constructor with parameters
+    public UserEntity(Long id, String username, String email, String phone, String password, Date createdAt, Date updatedAt) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt; 
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    // Override toString() method
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' + 
+                ", password='" + password + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+
+}
